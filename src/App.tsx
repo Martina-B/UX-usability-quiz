03dea702847f6e81a04ad58435188cdb9496df41
Marek Pastierik <marekpastierik15@gmail.com>
2021-11-30 22:07:34 +0100

    Remove artefacts from merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
-<<<<<<< HEAD
 import { ThemeProvider, CssBaseline } from '@mui/material';
-=======
-import { CssBaseline, ThemeProvider } from '@mui/material';
->>>>>>> c49833d6769e281be9ac8334244918ca32aa8f1e
 
 import Routes from './Components/Routes';
 import { UserProvider } from './Hooks/useLoggedInUser';
