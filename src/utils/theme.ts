c1fc5df0be067c278da7c0dc8546a60963d40b68
martina <456595@mail.muni.cz>
2021-11-30 20:48:55 +0100

    add theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,24 @@
+import { createTheme } from '@mui/material';
+
+declare module '@mui/material/styles' {
+	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+	interface Palette {
+		contrast?: string;
+	}
+	// eslint-disable-next-line @typescript-eslint/consistent-type-definitions
+	interface PaletteOptions {
+		contrast?: string;
+	}
+}
+
+export const theme = createTheme({
+	palette: {
+		primary: { main: '#2B2D42' }, //black-ish
+		secondary: { main: '#EDF2F4' }, //white-ish
+		contrast: '#FFF733', //yellow-ish
+
+		text: {
+			primary: '#2B2D42'
+		}
+	}
+});
