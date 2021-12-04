b7ee0cf2d7479d8881355b008e0a5fe849ac03e2
martina <456595@mail.muni.cz>
2021-12-04 18:52:27 +0100

    make Home more responsive

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,23 +1,32 @@
 import { Box, Button, Typography } from '@mui/material';
 import { Link } from 'react-router-dom';
+import '../Styles/Home.css';
 
 const Home = () => (
-	<Box sx={{ display: 'block', alignItems: 'left' }}>
+	<Box
+		sx={{
+			display: 'block',
+			alignItems: 'left',
+			position: 'relative',
+			justifyContent: 'center',
+			width: '200'
+		}}
+	>
 		<Button
 			component={Link}
 			to="/demo"
+			fullWidth
 			sx={{
 				'color': 'text.primary',
-				'fontSize': 90,
 				'fontWeight': 'black',
 				':hover': { color: 'contrast' }
 			}}
 		>
-			START QUIZ
+			<Typography variant="h1">START QUIZ</Typography>
 		</Button>
 		<Typography variant="h5">
-			*This quiz is about learning to recognize usability principles violation
-			in design.
+			This quiz is about learning to recognize usability principles violation in
+			design.
 		</Typography>
 	</Box>
 );
