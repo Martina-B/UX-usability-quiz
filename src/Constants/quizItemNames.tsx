b292d0a0bd80cf1790d612646b49bb63b439d983
martina <m.balakova@email.cz>
2021-12-05 12:40:35 +0100

    add more human-friendly labels into results

--- src/Constants/quizItemNames.tsx
+++ src/Constants/quizItemNames.tsx
@@ -1,25 +1,32 @@
 export const itemNamesTranslations: { [key: string]: string } = {
 	//PaymentModal
-	SendBtn: 'text here',
-	PaymentModalText: 'text here',
-	BathIcon: 'text here',
-	CloseBtn: 'text here',
+	SendBtn: 'OK button (wrongly selected)',
+	PaymentModalText: 'Payment was succesful label (wrongly selected)',
+	BathIcon:
+		'Bath Icon (not selected - violates Match between system and the real world)',
+	CloseBtn: 'Close button (not selected - violates Consistency and standards)',
 	//Error modal
-	DeleteBtn: 'text here',
-	ErrorModalText: 'text here',
-	SmileIcon: 'text here',
+	DeleteBtn:
+		'Delete my banking account button (not selected - violates User control and freedom)',
+	ErrorModalText:
+		'Error: java.io.IOException label (not selected - violates Match between system and the real world)',
+	SmileIcon: 'Sad smile icon (wrongly selected)',
 	//Payment form
-	Header: 'text here',
-	BulletPoint1: 'text here',
-	BulletPoint2: 'text here',
-	EmailLbl: 'text here',
-	EmailTB: 'text here',
-	CardLbl: 'text here',
-	CardTB: 'text here',
-	CardExpTB: 'text here',
-	CardCvcTB: 'text here',
-	CardNameLbl: 'text here',
-	CardNameTB: 'text here',
-	CardCountryLbl: 'text here',
-	CardCountryTB: 'text here'
+	Header: 'Banking app of the future header (wrongly selected)',
+	BulletPoint1: 'Keep your finances safe bullet point (wrongly selected)',
+	BulletPoint2:
+		'Customizable bulletpoint (not selected - low contrast of yellow writting on yellow background)',
+	EmailLbl: 'Email label (wrongly selected)',
+	EmailTB: 'Email editbox (wrongly selected)',
+	CardLbl: 'Card information label (wrongly selected)',
+	CardTB:
+		'Card information editbox (not selected - missing placeholder which violates Error prevention',
+	CardExpTB: 'Card expiration editbox (wrongly selected)',
+	CardCvcTB: 'CVC editbox (wrongly selected)',
+	CardNameLbl: 'Name on card label (wrongly selected)',
+	CardNameTB: 'Name on card editbox (wrongly selected)',
+	CardCountryLbl: 'Country of region label (wrongly selected)',
+	CardCountryTB:
+		'Country name editbox (not selected - it is editbox not selectbox which violates Recognition rather than recall',
+	SignInBtn: 'Pay $55.00 button (wrongly selected)'
 };
